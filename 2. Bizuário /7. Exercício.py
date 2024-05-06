67b40e7bb7968f089ffa00fa11476d7d66deb6e5
Marcelo Melo <160616710+MarceloMelo201@users.noreply.github.com>
2024-05-06 22:25:25 +0000

    Aulas

--- 2. Bizuário /7. Exercício.py
+++ 2. Bizuário /7. Exercício.py
@@ -4,3 +4,15 @@ menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
 """
 
+nome = input("Digite seu nome:  ")
+
+nomeLido = len(nome)
+
+if nomeLido <= 4:
+    print("Seu nome é muito curto")
+
+elif nomeLido > 4 and nomeLido < 7:
+    print("Seu nome é normal")
+
+else:
+    print("Seu nome é muito grande")
