313b807feb794f6debaf3efe362c4952d3f0cbdb
Marcelo Melo <160616710+MarceloMelo201@users.noreply.github.com>
2024-05-06 13:57:01 +0000

    Aulas

--- 1. Aulas.py/1.4 Exercício.py
+++ 1. Aulas.py/1.4 Exercício.py
@@ -1,1 +1,16 @@
-a
+from os import system 
+
+system("clear")
+login = input("Digite o login: ")
+senha = input("Digite a senha: ")
+
+nome = 'Marcelo'
+verificação = 'striker201'
+
+system("clear")
+
+if login == nome and senha == verificação:
+    print("Bem-vindo!")
+
+else:
+    print("Acesso não autorizado, login ou senha inválidos.")
