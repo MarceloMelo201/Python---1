7896951d8edaf7e7df038f512d88f4e8259bbf73
Marcelo Melo <160616710+MarceloMelo201@users.noreply.github.com>
2024-05-07 17:14:43 +0000

    Aulas

---
+++ 2. Bizuário /12. For_In.py
@@ -0,0 +1,28 @@
+# senha_salva = '123456'
+# senha_digitada = ''
+# repeticoes = 0
+
+# while senha_salva != senha_digitada:
+#     senha_digitada = input(f'Sua senha ({repeticoes}x): ')
+
+#     repeticoes += 1
+
+# print(repeticoes)
+# print('Aquele laço acima pode ter repetições infinitas')
+texto = 'Python'
+
+novo_texto = ''
+
+for letra in texto: #Para cada letra no texto
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto + '*')
+
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+    print(numero)
