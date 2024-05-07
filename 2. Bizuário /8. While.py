4108834dab6106820396b67634ec54eb880890a3
Marcelo Melo <160616710+MarceloMelo201@users.noreply.github.com>
2024-05-07 01:40:36 +0000

    Aulas

--- 2. Bizuário /8. While.py
+++ 2. Bizuário /8. While.py
@@ -92,3 +92,23 @@ print('Acabou')
 
 for i in range(3):
     print("f")
+
+"""                         while/else 
+
+"""
+    
+
+string = 'Valor qualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei um espaço na string.')
+print('Fora do while.')
