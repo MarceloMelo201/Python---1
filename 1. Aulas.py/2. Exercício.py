dfbe5cab18f14a6e98c88e77bcdf0be056d8f78f
Marcelo Melo <160616710+MarceloMelo201@users.noreply.github.com>
2024-05-10 12:53:57 +0000

    Aulas

---
+++ 1. Aulas.py/2. Exercício.py
@@ -0,0 +1,14 @@
+from os import system
+
+nome = input("Digite o nome: ")
+sexo = input("Digite o sexo (M/F): ")
+estado_civil = input("Digite seu estado cívil: ")
+
+if (sexo.upper() == 'F' and estado_civil.upper() == 'CASADA'):
+    tempo = int(input("Digite o tempo de casada: "))
+
+
+print(f"Nome: {nome}")
+print(f"Sexo: {sexo}")
+print(f"Estado Civil: {estado_civil}")
+print(f"Tempo de casada: {tempo}")
